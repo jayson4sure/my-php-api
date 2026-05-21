3837b431cecf212aebb2f256524d3393d1c25db1
JayAr <46701139+jayson4sure@users.noreply.github.com>
2026-05-21 23:30:06 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,10 +1,46 @@
 <?php
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
-$username = $_POST['username'] ?? '';
+include 'db.php';
 
-echo json_encode([
-    "success" => true,
-    "username" => $username
-]);
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
+
+if (empty($username) || empty($password)) {
+    echo json_encode([
+        "success" => false,
+        "message" => "Username and password required"
+    ]);
+    exit;
+}
+
+$query = "SELECT * FROM users WHERE username = $1 AND password = $2";
+
+$result = pg_query_params(
+    $conn,
+    $query,
+    [$username, $password]
+);
+
+if (pg_num_rows($result) > 0) {
+
+    $user = pg_fetch_assoc($result);
+
+    echo json_encode([
+        "success" => true,
+        "message" => "Login successful",
+        "user" => [
+            "id" => $user['id'],
+            "username" => $user['username']
+        ]
+    ]);
+
+} else {
+
+    echo json_encode([
+        "success" => false,
+        "message" => "Invalid username or password"
+    ]);
+}
