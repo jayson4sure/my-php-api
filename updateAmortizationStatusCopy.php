d5c7e727574548a1429e97be15f4d3af0dea2ff1
JayAr <46701139+jayson4sure@users.noreply.github.com>
2026-05-24 19:53:48 +0800

    Update updateAmortizationStatusCopy.php

--- updateAmortizationStatusCopy.php
+++ updateAmortizationStatusCopy.php
@@ -13,6 +13,7 @@ header("Access-Control-Allow-Origin: *");
 // header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
